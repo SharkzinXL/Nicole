c8f8bccce3f346bd6af3d10de3343ae99e23c924
SharkzinXL <heitor.leite001@gmail.com>
2025-12-17 05:07:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,23 @@ function mostrar() {
   document.getElementById("amo").style.display = "block";
 }
 
-// Corações caindo
-const heartsContainer = document.getElementById("hearts");
+document.addEventListener("DOMContentLoaded", () => {
+  const heartsContainer = document.getElementById("hearts");
 
-function createHeart() {
-  const heart = document.createElement("div");
-  heart.classList.add("heart");
-  heart.innerHTML = "❤️";
-  heart.style.left = Math.random() * 100 + "vw";
-  heart.style.fontSize = Math.random() * 20 + 15 + "px";
-  heart.style.animationDuration = Math.random() * 3 + 4 + "s";
+  function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.innerHTML = "❤️";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = Math.random() * 20 + 15 + "px";
+    heart.style.animationDuration = Math.random() * 3 + 4 + "s";
 
-  heartsContainer.appendChild(heart);
+    heartsContainer.appendChild(heart);
 
-  setTimeout(() => {
-    heart.remove();
-  }, 7000);
-}
+    setTimeout(() => {
+      heart.remove();
+    }, 7000);
+  }
 
-setInterval(createHeart, 300);
+  setInterval(createHeart, 300);
+});
